0e145a7f0263b37f43d458011134af3fee7ae147
Austin Finlayson <finlaysona20@students.ecu.edu>
2023-09-14 12:39:57 -0400

    Finished rewriting sample code to use sklearn classifier, modofied to work with Lucas's looping code and plot

--- Homework 1/Homework1.py
+++ Homework 1/Homework1.py
@@ -1,94 +1,94 @@
-# Example of kNN implemented from Scratch in Python
-
 import pandas as pd
+import numpy as np
 import random
 import math
 import operator
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.metrics import accuracy_score
 import matplotlib.pyplot as plt
-import numpy as np
 
-def loadDataset(filename, split):
-    trainingSet = []
-    testSet = []
-    df = pd.read_csv(filename, header=None)
+
+
+def loadDataset(url, split):
+    trainingSet=[]
+    testSet=[]
+    df = pd.read_csv(url, header=None)
     array = df.to_numpy()
     random.shuffle(array)
-    training_len = int(len(array) * split)
+    training_len = int(len(array)*split)
     trainingSet = array[:training_len]
     testSet = array[training_len:]
     return trainingSet, testSet
 
-def euclideanDistance(instance1, instance2, length):
-    distance = 0
-    for x in range(length):
-        distance += pow((instance1[x] - instance2[x]), 2)
-    return math.sqrt(distance)
-
-def getNeighbors(trainingSet, testInstance, k):
-    distances = []
-    length = len(testInstance) - 1
-    for x in range(len(trainingSet)):
-        dist = euclideanDistance(testInstance, trainingSet[x], length)
-        distances.append((trainingSet[x], dist))
-    distances.sort(key=operator.itemgetter(1))
-    neighbors = []
-    for x in range(k):
-        neighbors.append(distances[x][0])
-    return neighbors
-
-def getResponse(neighbors):
-    classVotes = {}
-    for x in range(len(neighbors)):
-        response = neighbors[x][-1]
-        if response in classVotes:
-            classVotes[response] += 1
-        else:
-            classVotes[response] = 1
-    sortedVotes = sorted(classVotes.items(), key=operator.itemgetter(1), reverse=True)
-    return sortedVotes[0][0]
-
-def getAccuracy(testSet, predictions):
-    correct = 0
-    for x in range(len(testSet)):
-        if testSet[x][-1] == predictions[x]:
-            correct += 1
-    return (correct / float(len(testSet))) * 100.0
-
-def main():
+"""
+This function translates the target strings into integers.
+targets: a list containing the iris data set
+targetNames: a list of the iris target types
+targetIndex: the index of the target name string in the targets list
+"""
+def parseTargets(targets, targetNames, targetIndex):
+    
+    if(len(targetNames) >= 2):
+        for target in targets:
+            if(target[targetIndex] == targetNames[0]):
+                target[targetIndex] = 0
+            elif(target[targetIndex] == targetNames[1]):
+                target[targetIndex] = 1
+            elif(target[targetIndex] == targetNames[2]):
+                target[targetIndex] = 2
+            else:
+                print(target)
+    else:
+        print("Target names size error")
+    
+    return [i[4] for i in targets]
+    
+def classifier(k):
     # prepare data
-    trainingSet = []
-    testSet = []
+    trainingSet=[]
+    testSet=[]
     split = 0.67
     url = 'https://raw.githubusercontent.com/ruiwu1990/CSCI_4120/master/KNN/iris.data'
+    print("\tLoading dataset...")
+    trainingSet, testSet = loadDataset(url, 0.66)
+    #print('Train set: ' + repr(len(trainingSet)))
+    #print('Test set: ' + repr(len(testSet)))
+    
+    targetNames = ['Iris-setosa', 'Iris-versicolor', 'Iris-virginica']
+    print("\tPreparing training data...")
+    trainX = [i[0:4] for i in trainingSet]
+    trainY = parseTargets(trainingSet, targetNames, 4)
+    print("\tPreparing testing data...")
+    testX = [i[0:4] for i in testSet]
+    testY = parseTargets(testSet, targetNames, 4)
+    
+    print("\tGenerating predictions...")
     # generate predictions
-    predictions = []
+    predictions=[]
+    result = KNeighborsClassifier(n_neighbors = k)
+    result.fit(trainX, trainY)
+    predictions = result.predict(testX)
+    print("\tComputing accuracy...")
+    accuracy = accuracy_score(predictions, testY)
+    print('\tAccuracy: ' + repr(np.around(accuracy,4)*10*10) + '%')
+    return accuracy
+    
+def main():
     kAccuracies = []
     totalAccuracy = 0
-    for k in range(20):
+    for k in range(1,20):
+        print("Running classifier for k = " + str(k))
         totalAccuracy = 0
         for attempt in range(5):
             accuracy = 0
-            trainingSet, testSet = loadDataset(url, split)
-            #print('Train set: ' + repr(len(trainingSet)))
-            #print('Test set: ' + repr(len(testSet)))
-            for x in range(len(testSet)):
-                # TODO starts here
-                # get neighbor between current test record and all training datasets
-                neighbors = getNeighbors(trainingSet, testSet[x], k+1)
-                # get response
-                result = getResponse(neighbors)
-                # append current prediction result to predictions list
-                predictions.append(result)
-                # print('> predicted=' + repr(result) + ', actual=' + repr(testSet[x][-1]))
-                # TODO ends here
-            accuracy = getAccuracy(testSet, predictions)
+            accuracy = classifier(k)
             totalAccuracy += accuracy
-            print('Accuracy: ' + repr(accuracy) + '%')
+            #print('Accuracy: ' + repr(accuracy) + '%')
         avgAccuracy = totalAccuracy/5
-        print('Accuracy for k={} is {}'.format(k+1, avgAccuracy))
+        print('\tAccuracy for k={} is {}'.format(k+1, avgAccuracy))
         kAccuracies.append(avgAccuracy)
 
-    x = np.arange(1,21)
+    x = np.arange(1,20)
     plt.plot(x, kAccuracies)
     plt.show()
 
