078005360a7b3068f5dc01330fc6c7dd871e5d1d
Austin Finlayson <finlaysona20@students.ecu.edu>
2023-10-05 14:48:39 -0400

    Finished code for hw2

--- Homework 2/hw2.py
+++ Homework 2/hw2.py
@@ -3,24 +3,29 @@ from sklearn.datasets import make_blobs
 from yellowbrick.cluster import KElbowVisualizer
 from sklearn.metrics import accuracy_score
 from sklearn.metrics import confusion_matrix
+from sklearn.manifold import TSNE
+import numpy as np
+from scipy.stats import mode
 
 X, y_true = make_blobs(n_samples=300, centers=4, cluster_std=0.60, random_state=0)
 
-kmeans = KMeans()
-visualizer = KElbowVisualizer(kmeans, k=(2, 10))
+visualizer = KElbowVisualizer(KMeans(n_init = "auto"), k=(2, 11))
 visualizer.fit(X)
 
 # TODO: Determine the best K for K-means
 bestK = visualizer.elbow_value_
-bestKMeans = KMeans(n_clusters = bestK, random_state=0)
-bestKMeans.fit(X)
 
-# TODO: Calculate accuracy for the best K
-y_pred = bestKMeans.labels_
-accuracy = accuracy_score(y_true, y_pred)
+# TODO: Calculate accuracy for the best k
+tsne = TSNE(n_components=2, init='random', random_state=0)
+proj = tsne.fit_transform(X)
+bestKMeans = KMeans(n_clusters=bestK, random_state=0)
+clusters = bestKMeans.fit_predict(proj)
+
+print(clusters.shape)
+accuracy = accuracy_score(y_true, clusters)
 
 # TODO: Draw a confusion matrix
-matrix = confusion_matrix(y_true, y_pred)
+matrix = confusion_matrix(y_true, clusters)
 
 print(f"Best K: {bestK}")
 print(f"Best K Accuracy: {accuracy}")
